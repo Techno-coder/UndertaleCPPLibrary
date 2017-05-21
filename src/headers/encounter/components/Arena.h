70d799d6aad443edf14fd5def140125fe34eb2ca
Techno-coder <zhiyuanqi@hotmail.co.nz>
2017-05-21 13:52:59 +1200

    Add arena

---
+++ src/headers/encounter/components/Arena.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <memory>
+
+namespace sf {
+	class RenderTarget;
+}
+
+namespace ug {
+	class Arena {
+		struct Impl;
+		std::unique_ptr<Impl> impl;
+	public:
+		Arena();
+		void draw(sf::RenderTarget& target);
+		/**
+		 * Changes the size of the rectangle relative from the center
+		 * @param width The width
+		 * @param height The height
+		 */
+		void resize(float width, float height);
+		/**
+		 * Sets the position of the center of the arena
+		 * @param x Distance from left
+		 * @param y Distance from top
+		 */
+		void setPosition(float x, float y);
+	};
+}
+
---
+++ src/implementation/encounter/components/Arena.cpp
@@ -0,0 +1,33 @@
+#include <encounter/components/Arena.h>
+
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+
+struct ug::Arena::Impl {
+	sf::RectangleShape rectangle;
+};
+
+void ug::Arena::draw(sf::RenderTarget& target) {
+	target.draw(impl->rectangle);
+}
+
+ug::Arena::Arena() : impl(std::make_unique<Impl>()) {
+	impl->rectangle.setFillColor(sf::Color::Black);
+	impl->rectangle.setOutlineColor(sf::Color::White);
+	impl->rectangle.setOutlineThickness(5);
+	resize(570, 130);
+	setPosition(320, 385);
+}
+
+void ug::Arena::resize(float width, float height) {
+	float widthDiff = impl->rectangle.getSize().x - width;
+	float heightDiff = impl->rectangle.getSize().y - height;
+	impl->rectangle.setSize(sf::Vector2f(width, height));
+	impl->rectangle.setOrigin(width / 2, height / 2);
+	impl->rectangle.move(widthDiff / 2, heightDiff / 2);
+}
+
+void ug::Arena::setPosition(float x, float y) {
+	impl->rectangle.setPosition(x, y);
+}
+
